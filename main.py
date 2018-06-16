603d5e44ae2246bdcfa20e2bcd523bcddf6feffe
C <39905165+Tnutlyrehc@users.noreply.github.com>
2018-06-16 15:30:39 +0200

    We count stuff
    and index it

--- main.py
+++ main.py
@@ -5,6 +5,9 @@
 #from keras.layers import LSTM
 #from keras.callbacks import ModelCheckoint
 #from keras.utils import np_utils
+from collections import Counter
+import pickle
+#pickle module for serialising and de-serialisring python projects
 
 #Load .txt file with aprox 150 Donald Trump tweet (June 3rd - June 16th)
 filename = open('trumptweets.txt') #encoding='uft-8')
@@ -27,5 +30,35 @@ unique_chars = len(chars_set)
 #char frequencies
 chars_frequency_dictionary = Counter(data)
 char_list_by_frequency = list(chars_frequency_dictionary.keys())
+#print(char_list_by_frequency)
+
+char_index = list(range(0, len(chars_set)))
+char_index = [i+1 for i in char_index]
+print(char_index)
+
+char_dictionary = {}
+for key, value in zip(char_list_by_frequency, char_index):
+    char_dictionary[key] = []
+    char_dictionary[key].append(value)
+print(char_dictionary)
+
+index_dictionary = {}
+for key, value in zip(char_index, char_list_by_frequency):
+    index_dictionary[key] = []
+    index_dictionary[key].append(value)
+
+#saving dictionaries
+with open('char_dictionary.pkl', "wb") as f:
+    pickle.dump(char_dictionary, f)
+
+with open('char_index_dictionary.pkl', "wb") as f:
+    pickle.dump(index_dictionary, f)
+
+
+
+
+
+
+
 
 
